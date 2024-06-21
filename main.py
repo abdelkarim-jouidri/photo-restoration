513d6ddf88acf4ffd446e37077147fdd68284053
abdelkrim-joudri <abdelkrimjoudri@gmail.com>
2024-06-21 05:12:44 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import replicate
+from dotenv import load_dotenv
+import base64
+load_dotenv()
+
+with open(r"C:\Users\hp\Documents\freelance\LR.jpg", 'rb') as file:
+  data = base64.b64encode(file.read()).decode('utf-8')
+  image = f"data:application/octet-stream;base64,{data}"
+
+input = {
+    "image": image,
+}
+
+output = replicate.run(
+    "jingyunliang/swinir:660d922d33153019e8c263a3bba265de882e7f4f70396546b6c9c8f9d47a021a",
+    input=input
+)
+print(output)
+#=> [{"file":"https://replicate.delivery/mgxm/1e3c0b87-01a7-4...
